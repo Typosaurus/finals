d78570ab6f8a33a13e07f90a2b088294669e2d2d
Typosaurus <144977733+Typosaurus@users.noreply.github.com>
2025-06-07 17:41:47 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyA44ldoRDGJbcbAHZub8zadurb2gukQA2s",
+  authDomain: "crud-7de36.firebaseapp.com",
+  databaseURL: "https://crud-7de36-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crud-7de36",
+  storageBucket: "crud-7de36.firebasestorage.app",
+  messagingSenderId: "837443992381",
+  appId: "1:837443992381:web:4124e1d4c7effa69996354"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
